0773413529dee154f62baeb05a5e0a2986e6edc8
DimaOrekhov <d.i.orekhov@gmail.com>
2020-03-08 21:00:16 +0300

    First commit

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -3,6 +3,7 @@ module Combinators where
 import           AST                 (AST (..), Operator (..))
 import           Control.Applicative (Alternative (..))
 import           Text.Printf         (printf)
+import           Control.Monad (liftM, ap)
 
 data Result error input result
   = Success input result
@@ -13,28 +14,33 @@ newtype Parser error input result
   = Parser { runParser :: input -> Result error input result}
 
 instance Functor (Parser error input) where
-  fmap = error "fmap not implemented"
+  fmap = liftM
 
 instance Applicative (Parser error input) where
-  pure = error "pure not implemented"
-  (<*>) = error "<*> not implemented"
+  pure = return
+  (<*>) = ap
 
 instance Monad (Parser error input) where
-  return = error "return not implemented"
-
-  (>>=) = error ">>= not implemented"
+  return x = Parser (\i -> Success i x)
+  (Parser m) >>= k = Parser go where
+    go inp = case m inp of
+               (Failure err) -> (Failure err)
+               (Success inp' x) -> runParser (k x) inp'
 
 instance Monoid error => Alternative (Parser error input) where
-  empty = error "empty not implemented"
-
-  (<|>) = error "<|> not implemented"
+  empty = Parser (\i -> Failure mempty)
+  (Parser p1) <|> (Parser p2) = Parser go where
+    go inp = case p1 inp of
+               (Failure _) -> p2 inp
+               s@(Success _ _) -> s
 
 -- Принимает последовательность элементов, разделенных разделителем
 -- Первый аргумент -- парсер для разделителя
 -- Второй аргумент -- парсер для элемента
 -- В последовательности должен быть хотя бы один элемент
-sepBy1 :: Parser e i sep -> Parser e i a -> Parser e i [a]
-sepBy1 sep elem = error "sepBy1 not implemented"
+sepBy1 :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i [a]
+sepBy1 sep elem = (:) <$> elem <*> (helper sep elem) where
+  helper sep elem = (:) <$> (sep *> elem) <*> helper sep elem <|> pure []
 
 -- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
 alt :: Parser e i a -> Parser e i a -> Parser e i a
--- src/UberExpr.hs
+++ src/UberExpr.hs
@@ -9,5 +9,19 @@ uberExpr :: Monoid e
          -> Parser e i ast
          -> (op -> ast -> ast -> ast)
          -> Parser e i ast
-uberExpr = error "uberExpr undefined"
+uberExpr [] elemParser _ = elemParser
+uberExpr ((opParser,assoc):ps) elemParser opFunc = 
+  let elemParser' = uberExpr ps elemParser
+      rightParser = ((\x f y -> opFunc f x y) <$> elemParser' <*> opParser <*> rightParser) <|> elemParser'
+      noAssocParser = undefined
+      leftParser = (do
+        left <- elemParser'
+        op <- opParser
+        right <- elemParser'
+        return $ opFunc op left right) <|> elemParser'
+  in case assoc of
+    LeftAssoc -> undefined
+    RightAssoc -> rightParser
+    NoAssoc -> undefined
 
+--parseRightAssoc opParser = ((\x f y -> BinOp
