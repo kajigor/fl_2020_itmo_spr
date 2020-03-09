7ede51054351d557a5a703bcd1e1cfdb12cd85db
Oleg Stotsky <ostotsky@gmaill.com>
2020-03-09 23:56:04 +0300

    hw3: refactor

--- src/UberExpr.hs
+++ src/UberExpr.hs
@@ -8,15 +8,7 @@ data Associativity
   | RightAssoc
   | NoAssoc
 
--- Есть парсер +, *
--- elementaryExpr - парсер термов
--- astConstructor - \op left right -> BinOp op left right
--- 4*3+5*6
--- 4 * 3 * 5
--- 3 < 4
-uberExpr ::
-     Monoid e => [(Parser e i op, Associativity)] -> Parser e i ast -> (op -> ast -> ast -> ast) -> Parser e i ast
-uberExpr [(opParser, assoc)] elementaryExprParser astConstructor =
+uberExprHelper (opParser, assoc) elementaryExprParser astConstructor =
   case assoc of
     LeftAssoc ->
       uncurry (foldl (\ast (op, ast') -> astConstructor op ast ast')) <$> sepBy1' opParser elementaryExprParser
@@ -30,20 +22,9 @@ uberExpr [(opParser, assoc)] elementaryExprParser astConstructor =
   where
     createRightAssoc xs = snd $ foldr1 (\val (_, acc) -> convert val acc) xs
     convert (Just sep, v) acc = (Just sep, astConstructor sep v acc)
-    convert (Nothing, v) _ = (Nothing, v)
-uberExpr ((opParser, assoc):xs) elementaryExprParser astConstructor =
-  case assoc of
-    LeftAssoc ->
-      uncurry (foldl (\ast (op, ast') -> astConstructor op ast ast')) <$>
-      sepBy1' opParser (uberExpr xs elementaryExprParser astConstructor)
-    RightAssoc -> createRightAssoc <$> sepBy1'' opParser (uberExpr xs elementaryExprParser astConstructor)
-    NoAssoc -> binOp <|> single
-      where binOp = do
-              left <- uberExpr xs elementaryExprParser astConstructor
-              op <- opParser
-              astConstructor op left <$> uberExpr xs elementaryExprParser astConstructor
-            single = uberExpr xs elementaryExprParser astConstructor
-  where
-    createRightAssoc xs = snd $ foldr1 (\val (_, acc) -> convert val acc) xs
-    convert (Just sep, v) acc = (Just sep, astConstructor sep v acc)
     convert (Nothing, v) _    = (Nothing, v)
+
+uberExpr xs@[(opParser, assoc)] elementaryExprParser astConstructor =
+  uberExprHelper (opParser, assoc) elementaryExprParser astConstructor
+uberExpr ((opParser, assoc):xs) elementaryExprParser astConstructor =
+  uberExprHelper (opParser, assoc) (uberExpr xs elementaryExprParser astConstructor) astConstructor
