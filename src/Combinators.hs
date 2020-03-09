1114365b549bcad9ac654510cf7a74def0c1ce28
valeria <urgbal@gmail.com>
2020-03-09 22:57:57 +0300

    coding task

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -1,5 +1,6 @@
 module Combinators where
 
+import           Control.Monad (liftM, ap)
 import           AST                 (AST (..), Operator (..))
 import           Control.Applicative (Alternative (..))
 import           Text.Printf         (printf)
@@ -13,44 +14,54 @@ newtype Parser error input result
   = Parser { runParser :: input -> Result error input result}
 
 instance Functor (Parser error input) where
-  fmap = error "fmap not implemented"
+--map'
+  fmap f p = Parser $ \input ->
+  	case runParser p input of
+    	Success i a -> Success i (f a)
+    	Failure e   -> Failure e
 
 instance Applicative (Parser error input) where
-  pure = error "pure not implemented"
-  (<*>) = error "<*> not implemented"
+  pure = return
+  (<*>) = ap
 
 instance Monad (Parser error input) where
-  return = error "return not implemented"
-
-  (>>=) = error ">>= not implemented"
+--return'
+  return x = Parser $ \input -> Success input x
+--seq'
+  (>>=) p f = Parser $ \input ->
+  	case runParser p input of
+    		Success i a -> runParser (f a) i
+    		Failure e   -> Failure e
 
 instance Monoid error => Alternative (Parser error input) where
-  empty = error "empty not implemented"
+--fail'
+  empty = Parser $ \input -> Failure mempty
 
-  (<|>) = error "<|> not implemented"
+  (<|>) p q = Parser $ \input ->
+  	case runParser p input of
+    		Failure _ -> runParser q input
+    		x         -> x
 
 -- Принимает последовательность элементов, разделенных разделителем
 -- Первый аргумент -- парсер для разделителя
 -- Второй аргумент -- парсер для элемента
 -- В последовательности должен быть хотя бы один элемент
-sepBy1 :: Parser e i sep -> Parser e i a -> Parser e i [a]
-sepBy1 sep elem = error "sepBy1 not implemented"
+sepBy1 :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i [a]
+sepBy1 sep elem = do
+	x <- elem
+	xs <- many' (sep >> elem)
+	return (x:xs)
 
--- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
-alt :: Parser e i a -> Parser e i a -> Parser e i a
-alt p q = Parser $ \input ->
-  case runParser p input of
-    Failure _ -> runParser q input
-    x         -> x
+sepBy1' :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i (a, [(sep, a)])
+sepBy1' sep elem = do
+	x <- elem
+	let sepHelper sep elem = do
+		s <- sep
+		e <- elem
+		return (s, e)
+	xs <- many' (sepHelper sep elem) 
+	return (x, xs)
 
--- Последовательное применение парсеров:
--- если первый парсер успешно принимает префикс строки, второй запускается на суффиксе.
--- Второй парсер использует результат первого.
-seq' :: Parser e i a -> (a -> Parser e i b) -> Parser e i b
-seq' p f = Parser $ \input ->
-  case runParser p input of
-    Success i a -> runParser (f a) i
-    Failure e   -> Failure e
 
 -- Проверяет, что первый элемент входной последовательности удовлетворяет предикату
 satisfy :: (a -> Bool) -> Parser String [a] a
@@ -58,6 +69,10 @@ satisfy p = Parser $ \input ->
   case input of
     (x:xs) | p x -> Success xs x
     input        -> Failure $ "Predicate failed"
+
+-- Всегда завершается ошибкой
+fail' :: e -> Parser e i a
+fail' e = Parser $ \input -> Failure e
 
 -- Успешно завершается, если последовательность содержит как минимум один элемент
 elem' :: Parser String [a] a
@@ -67,26 +82,11 @@ elem' = satisfy (const True)
 symbol :: (Eq a) => a -> Parser String [a] a
 symbol c = satisfy (==c)
 
--- В случае успешного разбора модифицирует результат при помощи функции f
-map' :: (a -> b) -> Parser e i a -> Parser e i b
-map' f p = Parser $ \input ->
-  case runParser p input of
-    Success i a -> Success i (f a)
-    Failure e   -> Failure e
-
--- Всегда завершается ошибкой
-fail' :: e -> Parser e i a
-fail' e = Parser $ \input -> Failure e
-
--- Всегда завершается успехом, вход не читает, возвращает данное значение
-return' :: a -> Parser e i a
-return' x = Parser $ \input -> Success input x
-
 -- Последовательное применение одного и того же парсера 0 или более раз
-many' :: Parser e i a -> Parser e i [a]
-many' p = some' p `alt` return' []
+many' :: Monoid e => Parser e i a -> Parser e i [a]
+many' p = some' p <|> return []
 
 -- Последовательное применения одного и того же парсера 1 или более раз
-some' :: Parser e i a -> Parser e i [a]
-some' p = p `seq'` \r -> map' (r:) (some' p `alt` return' [])
+some' :: Monoid e => Parser e i a -> Parser e i [a]
+some' p = p >>= \r -> fmap (r:) (some' p <|> return [])
 
--- src/Expr.hs
+++ src/Expr.hs
@@ -1,55 +1,62 @@
 module Expr where
 
 import           AST         (AST (..), Operator (..))
-import           Combinators (Parser (..), Result (..), alt, elem', fail', map',
-                              return', satisfy, seq', symbol)
+import           Combinators (Parser (..), Result (..), sepBy1', elem', satisfy, symbol, fail')
 import           Data.Char   (isDigit, digitToInt)
+import           Control.Applicative (empty, (<|>))
 
 -- Парсер для произведения/деления термов
 parseMult :: Parser String String AST
-parseMult = error "parseMult not implemented"
+parseMult = parse <|> parseTerm
+	where parse = do
+		(elem, rest) <- sepBy1' parseMultOp parseTerm
+		return (foldl (\x (y, z) -> BinOp y x z) elem rest)
+
+parseMultOp :: Parser String String Operator	
+parseMultOp = (symbol '*' <|> symbol '/') >>= toOperator
 
 -- Парсер для сложения/вычитания множителей
 parseSum :: Parser String String AST
-parseSum = error "parseSum not implemented"
+parseSum = parse <|> parseMult
+	where parse = do
+		(elem, rest) <- sepBy1' parseSumOp parseMult
+		return (foldl (\x (y, z) -> BinOp y x z) elem rest)
+		
+parseSumOp :: Parser String String Operator	
+parseSumOp = (symbol '+' <|> symbol '-') >>= toOperator
 
 -- Парсер чисел
 parseNum :: Parser String String Int
 parseNum =
-    map' toNum go
+    fmap toNum go
   where
     digit = satisfy isDigit
-    empty' = return' []
+    empty' = return []
     toNum = foldl (\acc d -> 10 * acc + digitToInt d) 0
     go =
-      digit `seq'`
-      \d -> map' (d:) (go `alt` empty')
+      digit >>= \d -> fmap (d:) (go <|> empty')
 
 -- Парсер для операторов
 parseOp :: Parser String String Operator
-parseOp = elem' `seq'` toOperator
+parseOp = elem' >>= toOperator
 
 -- Преобразование символов операторов в операторы
 toOperator :: Char -> Parser String String Operator
-toOperator '+' = return' Plus
-toOperator '*' = return' Mult
-toOperator '-' = return' Minus
-toOperator '/' = return' Div
+toOperator '+' = return Plus
+toOperator '*' = return Mult
+toOperator '-' = return Minus
+toOperator '/' = return Div
 toOperator _   = fail' "Failed toOperator"
 
 -- Парсер для терма: либо число, либо выражение в скобках.
 -- Скобки не хранятся в AST за ненадобностью.
 parseTerm :: Parser String String AST
-parseTerm =
-    map' Num parseNum `alt`
-    (lbr `seq'` \_ ->
-     parseTerm `seq'` \e ->
-     rbr `seq'` \_ ->
-     return' e
-    )
-  where
-    lbr = symbol '('
-    rbr = symbol ')'
+parseTerm = (fmap Num parseNum) <|> parseBrackets 
+	where parseBrackets = do
+		symbol '('
+		x <- parseExpr
+		symbol ')'
+		return x
 
 -- Парсер арифметических выражений над целыми числами с операциями +,-,*,/.
 parseExpr :: Parser String String AST
