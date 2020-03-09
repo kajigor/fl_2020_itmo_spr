ef05bfbeccda886b0a770e720cbdfe3ce148fbf7
Artem Bobrov <artembobrov3059@gmail.com>
2020-03-09 23:31:02 +0300

    hw3

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,69 +1,65 @@
 module Expr where
 
-import           AST         (AST (..), Operator (..))
-import           Combinators (Parser (..), Result (..), alt, elem', fail', map',
-                              return', satisfy, seq', symbol)
-import           Data.Char   (isDigit, digitToInt)
+import           AST                 (AST (..), Operator (..))
+import           Combinators         (Parser (..), Result (..), elem', nat,
+                                      satisfy, sepBy1', symbol)
+import           Control.Applicative
+import           Data.Char           (digitToInt, isDigit)
+import           UberExpr            (Associativity (..), uberExpr)
 
 -- Парсер для произведения/деления термов
 parseMult :: Parser String String AST
-parseMult = error "parseMult not implemented"
+parseMult = uberExpr [(parseMultDivOp, LeftAssoc)] parseTerm BinOp
+  where
+    parseMultDivOp = (symbol '*' <|> symbol '/') >>= toOperator
 
 -- Парсер для сложения/вычитания множителей
 parseSum :: Parser String String AST
-parseSum = error "parseSum not implemented"
+parseSum = uberExpr [(parseSumSub, LeftAssoc)] parseMult BinOp
+  where
+    parseSumSub = (symbol '+' <|> symbol '-') >>= toOperator
 
 -- Парсер чисел
 parseNum :: Parser String String Int
-parseNum =
-    map' toNum go
-  where
-    digit = satisfy isDigit
-    empty' = return' []
-    toNum = foldl (\acc d -> 10 * acc + digitToInt d) 0
-    go =
-      digit `seq'`
-      \d -> map' (d:) (go `alt` empty')
+parseNum = nat
 
 -- Парсер для операторов
 parseOp :: Parser String String Operator
-parseOp = elem' `seq'` toOperator
+parseOp = elem' >>= toOperator
 
 -- Преобразование символов операторов в операторы
 toOperator :: Char -> Parser String String Operator
-toOperator '+' = return' Plus
-toOperator '*' = return' Mult
-toOperator '-' = return' Minus
-toOperator '/' = return' Div
+toOperator '+' = return Plus
+toOperator '*' = return Mult
+toOperator '-' = return Minus
+toOperator '/' = return Div
 toOperator _   = fail' "Failed toOperator"
+
+fail' :: e -> Parser e i a
+fail' e = Parser $ \input -> Failure e
 
 -- Парсер для терма: либо число, либо выражение в скобках.
 -- Скобки не хранятся в AST за ненадобностью.
 parseTerm :: Parser String String AST
-parseTerm =
-    map' Num parseNum `alt`
-    (lbr `seq'` \_ ->
-     parseTerm `seq'` \e ->
-     rbr `seq'` \_ ->
-     return' e
-    )
+parseTerm = Num <$> parseNum <|> (parseLeftBracket *> parseSum <* parseRightBracket)
   where
-    lbr = symbol '('
-    rbr = symbol ')'
+    parseLeftBracket = symbol '('
+    parseRightBracket = symbol ')'
 
 -- Парсер арифметических выражений над целыми числами с операциями +,-,*,/.
 parseExpr :: Parser String String AST
 parseExpr = parseSum
 
 compute :: AST -> Int
-compute (Num x) = x
-compute (BinOp Plus x y) = compute x + compute y
-compute (BinOp Mult x y) = compute x * compute y
+compute (Num x)           = x
+compute (BinOp Plus x y)  = compute x + compute y
+compute (BinOp Mult x y)  = compute x * compute y
 compute (BinOp Minus x y) = compute x - compute y
-compute (BinOp Div x y) = compute x `div` compute y
+compute (BinOp Div x y)   = compute x `div` compute y
 
 evaluate :: String -> Maybe Int
-evaluate input = do
+evaluate input =
   case runParser parseExpr input of
-    Success rest ast | null rest -> return $ compute ast
+    Success rest ast
+      | null rest -> return $ compute ast
     _ -> Nothing
--- src/UberExpr.hs
+++ src/UberExpr.hs
@@ -1,13 +1,30 @@
 module UberExpr where
 
-import           Combinators (Parser (..))
+import           Combinators         (Parser (..), sepBy1', sepBy1'')
+import           Control.Applicative ((<|>))
+import           Debug.Trace
 
-data Associativity = LeftAssoc | RightAssoc | NoAssoc
+data Associativity
+  = LeftAssoc
+  | RightAssoc
+  | NoAssoc
 
-uberExpr :: Monoid e
-         => [(Parser e i op, Associativity)]
-         -> Parser e i ast
-         -> (op -> ast -> ast -> ast)
-         -> Parser e i ast
-uberExpr = error "uberExpr undefined"
-
+uberExpr ::
+     (Monoid e, Show ast, Show op)
+  => [(Parser e i op, Associativity)]
+  -> Parser e i ast
+  -> (op -> ast -> ast -> ast)
+  -> Parser e i ast
+uberExpr [] ast _ = ast
+uberExpr ((p, LeftAssoc):pa) ast builder = do
+  (op, terms) <- sepBy1' p (uberExpr pa ast builder)
+  return $ foldl (\acc (op, term) -> builder op acc term) op terms
+uberExpr ((p, RightAssoc):pa) ast builder = do
+  (terms, op) <- sepBy1'' p (uberExpr pa ast builder)
+  return $ foldr (\(term, op) acc -> builder op term acc) op terms
+uberExpr ((p, NoAssoc):pa) ast builder = parse <|> ast
+  where
+    parse = do
+      lhs <- ast
+      op <- p
+      builder op lhs <$> ast
