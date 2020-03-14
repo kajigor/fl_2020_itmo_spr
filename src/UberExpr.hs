ad665206dc48471f24a0072d7b96bc840196a870
Liza <liza8361@yandex.ru>
2020-03-14 03:05:57 +0300

    fix parser

--- src/UberExpr.hs
+++ src/UberExpr.hs
@@ -11,13 +11,13 @@ uberExpr :: Monoid e
          -> (op -> ast -> ast -> ast)
          -> Parser e i ast
 
-uberExpr xs parseElem opp = if (null $ tail $ xs) then do 
-             case (snd $ head xs) of
+uberExpr xs parseElem opp = if (null xs) then parseElem else 
+              case (snd $ head xs) of
                 LeftAssoc -> do
-                            (elem, expr) <- sepBy1' (fst $ head xs) parseElem
+                            (elem, expr) <- sepBy1' (fst $ head xs) parseTail
                             return $ foldl (\x (y, z) -> opp y x z) elem expr
                 RightAssoc -> do
-                            (elem, expr) <- sepBy1' (fst $ head xs) parseElem
+                            (elem, expr) <- sepBy1' (fst $ head xs) parseTail
                             if (not $ null $ expr) then 
                               let exp = elem : (map snd (init  expr)) in
                               let ops = map fst expr in 
@@ -25,25 +25,11 @@ uberExpr xs parseElem opp = if (null $ tail $ xs) then do
                             else return $ elem
                 NoAssoc -> parseNoAssoc <|> parseElem where
                          parseNoAssoc = do
-                           elem1 <- parseElem
+                           elem1 <- parseTail
                            op <- (fst $ head xs)
-                           elem2 <- parseElem
+                           elem2 <- parseTail
                            return $ opp op elem1 elem2
-         else do
-              case (snd $ head xs) of
-                 LeftAssoc -> do
-                            (elem, expr) <- sepBy1' (fst $ head xs) (uberExpr (tail xs) parseElem opp)
-                            return $ foldl (\x (y, z) -> opp y x z) elem expr
-                 RightAssoc -> do
-                            (elem, expr) <- sepBy1' (fst $ head xs) (uberExpr (tail xs) parseElem opp)
-                            if (not $ null $ expr) then 
-                              let exp = elem : (map snd (init  expr)) in
-                              let ops = map fst expr in 
-                              return $ foldr (\(x, y) z -> opp x y z) (snd $ last $ expr) (zip ops exp)
-                            else return $ elem
-                 NoAssoc -> parseNoAssoc <|> (uberExpr (tail xs) parseElem opp) where
-                         parseNoAssoc = do
-                           elem1 <- (uberExpr (tail xs) parseElem opp)
-                           op <- (fst $ head xs)
-                           elem2 <- (uberExpr (tail xs) parseElem opp) 
-                           return $ opp op elem1 elem2
+              where 
+                parseTail = if (null $ tail $ xs) then parseElem 
+                            else (uberExpr (tail xs) parseElem opp)
+        
