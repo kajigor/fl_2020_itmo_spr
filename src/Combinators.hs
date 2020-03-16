b5f9d14ba134fe807220fd64dd437c8113b5726e
Artem Bobrov <artembobrov3059@gmail.com>
2020-03-16 23:27:05 +0300

    hw

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Combinators where
 
 import           AST                 (AST (..), Operator (..))
 import           Control.Applicative (Alternative (..))
+import           Control.Monad       (ap, liftM2)
+import           Data.Char           (digitToInt, isAlpha, isDigit)
+import           Debug.Trace         (traceShow)
 import           Text.Printf         (printf)
 
 data Result error input result
@@ -9,84 +14,112 @@ data Result error input result
   | Failure error
   deriving (Show, Eq)
 
-newtype Parser error input result
-  = Parser { runParser :: input -> Result error input result}
+newtype Parser error input result =
+  Parser
+    { runParser :: input -> Result error input result
+    }
 
 instance Functor (Parser error input) where
-  fmap = error "fmap not implemented"
+  fmap g (Parser p) = Parser f
+    where
+      f input =
+        case p input of
+          (Success input' result') -> Success input' (g result')
+          (Failure error)          -> Failure error
 
 instance Applicative (Parser error input) where
-  pure = error "pure not implemented"
-  (<*>) = error "<*> not implemented"
+  pure = return
+  (<*>) = ap
 
 instance Monad (Parser error input) where
-  return = error "return not implemented"
-
-  (>>=) = error ">>= not implemented"
+  return x = Parser $ \input -> Success input x
+  (Parser x) >>= k = Parser f
+    where
+      f input =
+        case x input of
+          Success input' result' -> runParser (k result') input'
+          Failure error          -> Failure error
 
 instance Monoid error => Alternative (Parser error input) where
-  empty = error "empty not implemented"
-
-  (<|>) = error "<|> not implemented"
-
--- Принимает последовательность элементов, разделенных разделителем
--- Первый аргумент -- парсер для разделителя
--- Второй аргумент -- парсер для элемента
--- В последовательности должен быть хотя бы один элемент
-sepBy1 :: Parser e i sep -> Parser e i a -> Parser e i [a]
-sepBy1 sep elem = error "sepBy1 not implemented"
-
--- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
-alt :: Parser e i a -> Parser e i a -> Parser e i a
-alt p q = Parser $ \input ->
-  case runParser p input of
-    Failure _ -> runParser q input
-    x         -> x
-
--- Последовательное применение парсеров:
--- если первый парсер успешно принимает префикс строки, второй запускается на суффиксе.
--- Второй парсер использует результат первого.
-seq' :: Parser e i a -> (a -> Parser e i b) -> Parser e i b
-seq' p f = Parser $ \input ->
-  case runParser p input of
-    Success i a -> runParser (f a) i
-    Failure e   -> Failure e
-
--- Проверяет, что первый элемент входной последовательности удовлетворяет предикату
-satisfy :: (a -> Bool) -> Parser String [a] a
-satisfy p = Parser $ \input ->
-  case input of
-    (x:xs) | p x -> Success xs x
-    input        -> Failure $ "Predicate failed"
-
--- Успешно завершается, если последовательность содержит как минимум один элемент
-elem' :: Parser String [a] a
-elem' = satisfy (const True)
-
--- Проверяет, что первый элемент входной последовательности -- данный символ
-symbol :: (Eq a) => a -> Parser String [a] a
-symbol c = satisfy (==c)
-
--- В случае успешного разбора модифицирует результат при помощи функции f
-map' :: (a -> b) -> Parser e i a -> Parser e i b
-map' f p = Parser $ \input ->
-  case runParser p input of
-    Success i a -> Success i (f a)
-    Failure e   -> Failure e
+  empty = Parser $ \input -> Failure mempty
+  Parser u <|> Parser v = Parser f
+    where
+      f input =
+        case u input of
+          Failure _ -> v input
+          x         -> x
 
 -- Всегда завершается ошибкой
 fail' :: e -> Parser e i a
 fail' e = Parser $ \input -> Failure e
 
--- Всегда завершается успехом, вход не читает, возвращает данное значение
-return' :: a -> Parser e i a
-return' x = Parser $ \input -> Success input x
+-- Принимает последовательность элементов, разделенных разделителем
+-- Первый аргумент -- парсер для разделителя
+-- Второй аргумент -- парсер для элемента
+-- В последовательности должен быть хотя бы один элемент
+sepBy1 :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i [a]
+sepBy1 sepP elP = do
+  elem <- elP
+  elems <- many $ sepP >> elP
+  return $ elem : elems
 
--- Последовательное применение одного и того же парсера 0 или более раз
-many' :: Parser e i a -> Parser e i [a]
-many' p = some' p `alt` return' []
+-- как sepBy1, только еще собирает разделители
+sepBy1' :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i (a, [(sep, a)])
+sepBy1' sepP elP = do
+  elem <- elP
+  elems <- many ((,) <$> sepP <*> elP)
+  return (elem, elems)
 
--- Последовательное применения одного и того же парсера 1 или более раз
-some' :: Parser e i a -> Parser e i [a]
-some' p = p `seq'` \r -> map' (r:) (some' p `alt` return' [])
+sepBy1'' :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i ([(a, sep)], a)
+sepBy1'' sepP elP = do
+  elems <- many ((,) <$> elP <*> sepP)
+  elem <- elP
+  return (elems, elem)
 
+-- Проверяет, что первый элемент входной последовательности удовлетворяет предикату
+satisfy :: (a -> Bool) -> Parser String [a] a
+satisfy p =
+  Parser $ \case
+    (x:xs)
+      | p x -> Success xs x
+    input -> Failure "Predicate failed"
+
+-- Успешно завершается, если последовательность содержит как минимум один элемент
+elem' :: Parser String [a] a
+elem' = satisfy (const True)
+
+many1 :: Monoid e => Parser e i a -> Parser e i [a]
+many1 p = liftM2 (:) p (many p)
+
+-- Проверяет, что первый элемент входной последовательности -- данный символ
+symbol :: (Eq a) => a -> Parser String [a] a
+symbol c = satisfy (== c)
+
+digit :: Parser String String Int
+digit = digitToInt <$> satisfy isDigit
+
+toNum :: [Int] -> Int
+toNum = foldl (\rec x -> x + 10 * rec) 0
+
+nat :: Parser String String Int
+nat = toNum <$> some digit
+
+eof :: Parser String String ()
+eof = Parser result
+  where
+    result []    = Success [] ()
+    result input = Failure "not empty input"
+
+letter :: Parser String String Char
+letter = satisfy (not . isSeparator)
+  where
+    isSeparator ch = ch == ' ' || ch == '\n'
+
+endLine :: Parser String String Char
+endLine = symbol '\n'
+
+word :: Parser String String String
+word = many1 letter
+
+space :: Parser String String Char
+space = symbol ' '
--- test/Test/Keyword.hs
+++ test/Test/Keyword.hs
@@ -1,24 +1,89 @@
 module Test.Keyword where
 
-import           Combinators      (Result (..), runParser)
-import           Control.Monad    (forM)
-import           Keyword          (keyword)
-import           Test.Tasty.HUnit (Assertion, (@?=))
+import Combinators (Result(..), runParser)
+import Control.Monad (forM)
+import Debug.Trace (traceShowId)
+import Keyword (keyword)
+import Test.Tasty.HUnit (Assertion, (@?=))
 
-kotlinKeywords = ["as", "as?", "break", "class", "continue", "do", "else", "false", "for", "fun", "if", "in", "!in", "interface", "is", "!is", "null", "object", "package", "return", "super", "this", "throw", "true", "try", "typealias", "typeof", "val", "var", "when", "while"]
+kotlinKeywords =
+  [ "as"
+  , "as?"
+  , "break"
+  , "class"
+  , "continue"
+  , "do"
+  , "else"
+  , "false"
+  , "for"
+  , "fun"
+  , "if"
+  , "in"
+  , "!in"
+  , "interface"
+  , "is"
+  , "!is"
+  , "null"
+  , "object"
+  , "package"
+  , "return"
+  , "super"
+  , "this"
+  , "throw"
+  , "true"
+  , "try"
+  , "typealias"
+  , "typeof"
+  , "val"
+  , "var"
+  , "when"
+  , "while"
+  ]
 
-cKeywords = ["auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"]
+cKeywords =
+  [ "auto"
+  , "break"
+  , "case"
+  , "char"
+  , "const"
+  , "continue"
+  , "default"
+  , "do"
+  , "double"
+  , "else"
+  , "enum"
+  , "extern"
+  , "float"
+  , "for"
+  , "goto"
+  , "if"
+  , "int"
+  , "long"
+  , "register"
+  , "return"
+  , "short"
+  , "signed"
+  , "sizeof"
+  , "static"
+  , "struct"
+  , "switch"
+  , "typedef"
+  , "union"
+  , "unsigned"
+  , "void"
+  , "volatile"
+  , "while"
+  ]
 
 haskellKeywords = ["as", "case", "of", "class", "data", "data family", "data instance", "default", "deriving", "deriving instance", "do", "forall", "foreign", "hiding", "if", "then", "else", "import", "infix", "infixl", "infixr", "instance", "let", "in", "mdo", "module", "newtype", "proc", "qualified", "rec", "type", "type family", "type instance", "where" ]
 
 isFailure (Failure _) = True
-isFailure  _          = False
+isFailure _ = False
 
 unit_Keywords :: Assertion
 unit_Keywords = do
   let suffix = "suffix"
   let prefix = "prefix"
-
   mapM_
     (\kw -> do
       mapM_ (\str -> runParser (keyword kw)  str @?= Success "" str) kw
