bf46f99135f9e9d4dbe0fff79c763750e7ebd252
Artem Bobrov <artembobrov3059@gmail.com>
2020-03-21 19:50:26 +0300

    hw4 cherry

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,34 +1,65 @@
 module Expr where
 
-import           AST         (AST (..), Operator (..))
-import           Combinators (Parser (..), Result (..))
-import           Data.Char   (isDigit, digitToInt)
+import           AST                 (AST (..), Operator (..))
+import           Combinators         (Parser (..), Result (..), elem', nat,
+                                      satisfy, sepBy1', symbol)
+import           Control.Applicative
+import           Data.Char           (digitToInt, isDigit)
+import           UberExpr            (Associativity (..), uberExpr)
 
--- Парсер арифметических выражений над целыми числами
-parseExpr :: Parser String String AST
-parseExpr = error "parseExpr undefined"
+-- Парсер для произведения/деления термов
+parseMult :: Parser String String AST
+parseMult = uberExpr [(parseMultDivOp, LeftAssoc)] parseTerm BinOp
+  where
+    parseMultDivOp = (symbol '*' <|> symbol '/') >>= toOperator
 
-parseIdent :: Parser String String String
-parseIdent = error "parseIdent undefined"
+-- Парсер для сложения/вычитания множителей
+parseSum :: Parser String String AST
+parseSum = uberExpr [(parseSumSub, LeftAssoc)] parseMult BinOp
+  where
+    parseSumSub = (symbol '+' <|> symbol '-') >>= toOperator
 
 -- Парсер чисел
 parseNum :: Parser String String Int
-parseNum = error "parseNum undefined"
+parseNum = nat
 
 -- Парсер для операторов
 parseOp :: Parser String String Operator
-parseOp = error "parseOp undefined"
+parseOp = elem' >>= toOperator
+
+-- Преобразование символов операторов в операторы
+toOperator :: Char -> Parser String String Operator
+toOperator '+' = return Plus
+toOperator '*' = return Mult
+toOperator '-' = return Minus
+toOperator '/' = return Div
+toOperator _   = fail' "Failed toOperator"
+
+fail' :: e -> Parser e i a
+fail' e = Parser $ \input -> Failure e
+
+-- Парсер для терма: либо число, либо выражение в скобках.
+-- Скобки не хранятся в AST за ненадобностью.
+parseTerm :: Parser String String AST
+parseTerm = Num <$> parseNum <|> (parseLeftBracket *> parseSum <* parseRightBracket)
+  where
+    parseLeftBracket = symbol '('
+    parseRightBracket = symbol ')'
+
+-- Парсер арифметических выражений над целыми числами с операциями +,-,*,/.
+parseExpr :: Parser String String AST
+parseExpr = parseSum
 
 compute :: AST -> Int
-compute (Num x) = x
-compute (BinOp Plus x y) = compute x + compute y
-compute (BinOp Mult x y) = compute x * compute y
+compute (Num x)           = x
+compute (BinOp Plus x y)  = compute x + compute y
+compute (BinOp Mult x y)  = compute x * compute y
 compute (BinOp Minus x y) = compute x - compute y
-compute (BinOp Div x y) = compute x `div` compute y
-compute _ = error "compute undefined"
+compute (BinOp Div x y)   = compute x `div` compute y
 
 evaluate :: String -> Maybe Int
-evaluate input = do
+evaluate input =
   case runParser parseExpr input of
-    Success rest ast | null rest -> return $ compute ast
+    Success rest ast
+      | null rest -> return $ compute ast
     _ -> Nothing
